7e50ce6ea7f54646f1c989fdbc813631a7766670
Simon Huang <huisimonliu@gmail.com>
2024-06-22 12:46:55 -0700

    User Adding name complete

--- app/home.js
+++ app/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { router } from 'expo-router';
 
+
 const home = () => {
     return (
         <View>
