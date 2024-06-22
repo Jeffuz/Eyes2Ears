3cb790a34efa8df9823330b04ea93d2bbcfb5fa7
Simon Huang <huisimonliu@gmail.com>
2024-06-22 15:01:23 -0700

    Permissions for audio in progress

--- app/home.js
+++ app/home.js
@@ -2,48 +2,19 @@ import React, {useEffect} from 'react'
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { router } from 'expo-router';
 import { PermissionsAndroid } from 'react-native';
-//import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 
 const home = () => {
 
      useEffect(() => {
-    //     request(PERMISSIONS.ANDROID.CAMERA)
-    //         .then(result => {
-    //             switch (result) { 
-    //                 case RESULTS.UNAVAILABLE:
-    //                     console.log('This feature is not available (on this device / in this context)');
-    //                     break;
-    //                   case RESULTS.DENIED:
-    //                     console.log('The permission has not been requested / is denied but requestable');
-    //                     break;
-    //                   case RESULTS.LIMITED:
-    //                     console.log('The permission is limited: some actions are possible');
-    //                     break;
-    //                   case RESULTS.GRANTED:
-    //                     console.log('The permission is granted');
-    //                     break;
-    //                   case RESULTS.BLOCKED:
-    //                     console.log('The permission is denied and not requestable anymore');
-    //                     break;
-    //             }
-    //         })
-    //         .catch((error) => console.error("ERROR", error))
         const requestCameraPermission = async () => {
             try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.CAMERA,
-                {
-                title: 'Cool Photo App Camera Permission',
-                message:
-                    'Cool Photo App needs access to your camera ' +
-                    'so you can take awesome pictures.',
-                buttonNeutral: 'Ask Me Later',
-                buttonNegative: 'Cancel',
-                buttonPositive: 'OK',
-                },
+            const permissionResult = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
             );
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+
+            console.log(permissionResult)
+            if (permissionResult === PermissionsAndroid.RESULTS.GRANTED) {
                 console.log('You can use the camera');
             } else {
                 console.log('Camera permission denied');
@@ -54,6 +25,7 @@ const home = () => {
         };
         requestCameraPermission();
     }, [])
+    
     const NEXT_SLIDE = 'scan';
     return (
         <Pressable onPress={() => router.push(NEXT_SLIDE)}>
