1a180489286d62c8f09e491615eb218e703e169d
Simon Huang <huisimonliu@gmail.com>
2024-06-22 15:49:49 -0700

    Stopped tracking .env

--- app/home.js
+++ app/home.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { router } from "expo-router";
 import OpenAI from "openai";
@@ -7,12 +7,13 @@ import { PermissionsAndroid } from 'react-native';
 
 const home = () => {
   const [currentDescription, setCurrentDescription] = useState(null);
-  
+
     useEffect(() => {
         const requestCameraPermission = async () => {
+
             try {
-            const permissionResult = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
+                const permissionResult = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
             );
 
             console.log(permissionResult)
@@ -28,63 +29,58 @@ const home = () => {
         requestCameraPermission();
     }, [])
     
+    const openai = new OpenAI({
+        apiKey: OPENAPI_KEY,
+      });
+    
+    const description = async () => {
+
+        console.log("PRESSED ", OPENAPI_KEY)
+        try {
+            const response = await openai.chat.completions.create({
+                model: "gpt-4o",
+                messages: [
+                {
+                    role: "user",
+                    content: [
+                    { type: "text", text: "Describe whats in the image" },
+                    {
+                        type: "image_url",
+                        image_url: {
+                        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                        },
+                    },
+                    ],
+                    },
+                ],
+            });
+            console.log(response.choices[0].message.content)
+        } catch (e) { console.log(e) }
+        
+    // setCurrentDescription(response.choices[0]);
+    };
+    
     const NEXT_SLIDE = 'scan';
     return (
-        <Pressable onPress={() => router.push(NEXT_SLIDE)}>
-            <View>
-                <Text>You are on home page going to scan page</Text>
-            </View>
+        // <Pressable onPress={() => router.push(NEXT_SLIDE)}>
+        //     <View>
+        //         <Text>You are on home page going to scan page</Text>
+        //     </View>
+
+        // </Pressable>
+        <Pressable onPress={() => description()}>
+          <Text>Set Description</Text>
+          <Text>Set Description</Text>
+          <Text>Set Description</Text>
+          <Text>Set Description</Text>
+          <Text>Set Description</Text>
+          <Text>Set Description</Text>
         </Pressable>
+        
     )
-}
 
-  const openai = new OpenAI({
-    apiKey: OPENAPI_KEY,
-  });
-
-  const description = async () => {
-    const response = await openai.chat.completions.create({
-      model: "gpt-4o",
-      messages: [
-        {
-          role: "user",
-          content: [
-            { type: "text", text: "Describe whats in the image" },
-            {
-              type: "image_url",
-              image_url: {
-                url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-              },
-            },
-          ],
-        },
-      ],
-    });
-    console.log(response.choices[0].message.content)
-    // setCurrentDescription(response.choices[0]);
-  };
-
-  return (
-    <View>
-      <Pressable onPress={() => router.push("scan")}>
-        <Text>You are on home page going to scan page</Text>
-      </Pressable>
-      <Text>.</Text>
-      <Text>.</Text>
-      <Text>.</Text>
-      <Text>.</Text>
-      <Text>.</Text>
-      <Pressable
-        onPress={() => {
-          description();
-        }}
-      >
-        <Text>Set Description</Text>
-      </Pressable>
-      <Text></Text>
-    </View>
-  );
-};
+    
+}; 
 
 export default home;
 
