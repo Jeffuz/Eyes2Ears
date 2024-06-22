b32e087cecffb2206104944c4eee6f4727ff656f
Simon Huang <huisimonliu@gmail.com>
2024-06-22 12:15:44 -0700

    Slides route properly

--- app/OnboardingPages/slide_1.js
+++ app/OnboardingPages/slide_1.js
@@ -1,15 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router';
 
 const slide_1 = () => {
-  const NEXT_SLIDE = './OnboardingPages/slide_2';
+  const NEXT_SLIDE = './slide_2';
   return (
     <Pressable onPress={() => router.push(NEXT_SLIDE)}>
-    <View>
-        <Text>On Slide 1</Text>
-        <Text>Press to Go Slide 2</Text>
-    </View>
-</Pressable>
+      <View>
+          <Text>On Slide 1</Text>
+          <Text>Press to Go Slide 2</Text>
+      </View>
+    </Pressable>
   )
 }
 
