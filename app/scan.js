928c0b595f1c70db0bee4f2d2fc2bd11cca42e3a
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-06-22 18:37:13 -0700

    return object of picture details

--- app/scan.js
+++ app/scan.js
@@ -1,67 +1,98 @@
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
-import { router } from 'expo-router';
+import { CameraView, useCameraPermissions, camera } from "expo-camera";
+import { useState, useRef } from "react";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import GestureRecognizer, {
+  swipeDirections,
+} from "react-native-swipe-gestures";
+import { router } from "expo-router";
 
 const scan = () => {
-    const [facing, setFacing] = useState('back');
-    const [permission, requestPermission] = useCameraPermissions();
+  const [facing, setFacing] = useState("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef(null);
 
+  if (!permission) {
+    return <View />;
+  }
 
-    if (!permission) {
-        return <View />;
-    }
-
-    if (!permission.granted) {
-        return (
-            <View style={styles.container}>
-                <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-                <Button onPress={requestPermission} title="grant permission" />
-            </View>
-        );
-    }
-
-    function toggleCameraFacing() {
-        setFacing(current => (current === 'back' ? 'front' : 'back'));
-    }
-
-    const onSwipe = (gestureName) => {
-        const { SWIPE_LEFT, SWIPE_RIGHT, SWIPE_DOWN } = swipeDirections;
-        if (gestureName === SWIPE_LEFT || gestureName === SWIPE_RIGHT) {
-            toggleCameraFacing();
-        }
-        if (gestureName === SWIPE_DOWN) {
-            router.push('home');
-        }
-    };
-
+  if (!permission.granted) {
     return (
-        <View style={styles.container}>
-            <GestureRecognizer
-                onSwipe={(direction) => onSwipe(direction)}
-                style={{
-                    flex: 1,
-                }}
-            >
-                <CameraView style={styles.camera} facing={facing}>
-                    <View>
-                        <TouchableOpacity onPress={toggleCameraFacing} />
-                    </View>
-                </CameraView>
-            </GestureRecognizer>
-        </View >
-    )
-}
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
 
-export default scan
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    } else {
+      console.log("L");
+    }
+  };
+
+  const onSwipe = (gestureName) => {
+    const { SWIPE_LEFT, SWIPE_RIGHT, SWIPE_DOWN, SWIPE_UP } = swipeDirections;
+    if (gestureName === SWIPE_LEFT || gestureName === SWIPE_RIGHT) {
+      toggleCameraFacing();
+    }
+    if (gestureName === SWIPE_DOWN) {
+      router.push("home");
+    }
+    if (gestureName === SWIPE_UP) {
+      takePicture();
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <GestureRecognizer
+        onSwipe={(direction) => onSwipe(direction)}
+        style={{
+          flex: 1,
+        }}
+      >
+        <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
+          {/* <Pressable
+            onPress={() => {
+              takePicture();
+            }}
+          > */}
+          <TouchableOpacity
+            onPress={() => {
+              toggleCameraFacing();
+            }}
+          />
+          {/* </Pressable> */}
+        </CameraView>
+      </GestureRecognizer>
+    </View>
+  );
+};
+
+export default scan;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    camera: {
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
 });
